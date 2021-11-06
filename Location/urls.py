8018c74f3517b6cdf0572f7ca28fbe9780433320
maxwell <shaxa882@gmail.com>
2021-11-07 02:23:09 +0500

    collect static

--- Location/urls.py
+++ Location/urls.py
@@ -13,15 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('userApp.urls')),
-    path('add/', include('userApp.urls')),
-    path('comment/', include('userApp.urls')),
-    path('location/', include('userApp.urls')),
-    path('specialist/', include('userApp.urls')),
+                  path('admin/', admin.site.urls),
+                  path('auth/', include('userApp.urls')),
+                  path('add/', include('userApp.urls')),
+                  path('comment/', include('userApp.urls')),
+                  path('location/', include('userApp.urls')),
+                  path('specialist/', include('userApp.urls')),
 
-]
+              ] \
+              + static(settings.STATIC_ROOT,
+                       document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL,
+                       document_root=settings.MEDIA_ROOT)
