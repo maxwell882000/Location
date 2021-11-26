5f9d327cf19f8a61ed87b77e9c6b405524fb8d84
maxwell <shaxa882@gmail.com>
2021-11-26 16:52:56 +0500

    fixed comment url

--- commentApp/urls.py
+++ commentApp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from commentApp.views import specialist_comment, specialist_review
+from commentApp.views import specialist_comment, specialist_review, location_comment, location_review
 
 urlpatterns = [
     path("specialist/<int:pk>/", specialist_comment),
-    path("specialist/list/", specialist_comment),
     path('specialist/review/', specialist_review),
-    path("location/<int:pk>/", specialist_comment),
-    path("location/list/", specialist_comment),
-    path('location/review/', specialist_review)
+    path("location/<int:pk>/", location_comment),
+    path('location/review/', location_review)
 ]
