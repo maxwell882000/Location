c84a08cd4dd58e9790f1c2dfdd91af1fb7d9b74a
maxwell <shaxa882@gmail.com>
2021-11-26 10:53:46 +0500

    search by category

--- locationApp/builders.py
+++ locationApp/builders.py
@@ -16,4 +16,8 @@ def location_builder(filter_by: dict):
         builder = builder & Q(radius_sqr__lte=pow(rad / 9, 2))
     if 'search' in filter_by:
         builder = builder & Q(district__contains=filter_by['search'][0])
+
+    if 'category' in filter_by:
+        id = int(filter_by['category'])
+        builder = builder & Q(specialist__category=id)
     return location.filter(builder)
