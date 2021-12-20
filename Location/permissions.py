43c8b2cc6317381c61bbf7cfe3d24a5f42e0da15
maxwell <shaxa882@gmail.com>
2021-12-20 22:25:02 +0500

    set custom code for phone error

--- Location/permissions.py
+++ Location/permissions.py
@@ -3,5 +3,6 @@ from rest_framework import permissions
 
 class CheckPhone(permissions.BasePermission):
     code = 410
+    message = "Вы не подтвердили свой номер телефона"
     def has_permission(self, request, view):
         return request.user.is_phone_validated
