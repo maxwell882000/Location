a7aecc800a6357fe09fbda44e5e79177010200a5
maxwell <shaxa882@gmail.com>
2021-12-20 22:24:06 +0500

    set custom code for phone error

--- Location/permissions.py
+++ Location/permissions.py
@@ -2,5 +2,6 @@ from rest_framework import permissions
 
 
 class CheckPhone(permissions.BasePermission):
+    code = 410
     def has_permission(self, request, view):
         return request.user.is_phone_validated
