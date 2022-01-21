6c979d8d99c92944586e58d7fb5e45189f309c11
maxwell <shaxa882@gmail.com>
2022-01-22 02:17:01 +0500

    Fixed

--- userApp/serializers.py
+++ userApp/serializers.py
@@ -90,7 +90,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         password = validated_data.pop('password')
         instance.set_password(password)
-        instance.update(**validated_data)
+        instance.firstname = validated_data.pop('firstname')
+        if validated_data['phone'] == instance.phone:
+            instance.phone = validated_data.pop('phone')
+        instance.lastname = validated_data.pop('lastname')
         instance.save()
         return instance
 
