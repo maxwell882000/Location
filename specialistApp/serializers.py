14ebe1e3cf5e808e5e589644f68ead3a775cbcf3
maxwell <shaxa882@gmail.com>
2022-01-22 02:05:34 +0500

    Fixed

--- specialistApp/serializers.py
+++ specialistApp/serializers.py
@@ -57,7 +57,7 @@ class SpecialistCreateSerializer(serializers.ModelSerializer):
 
 
 class SpecialistUpdateSerializer(serializers.ModelSerializer):
-    # user = RegisterSerializer(partial=True)
+    user = RegisterSerializer(partial=True)
     image = serializers.SerializerMethodField()
 
     class Meta:
