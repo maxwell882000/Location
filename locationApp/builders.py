1f5ac9e159a7fc17eed376d8b5a7bcad492fb76f
maxwell <shaxa882@gmail.com>
2022-01-24 19:47:36 +0500

    Fixed search

--- locationApp/builders.py
+++ locationApp/builders.py
@@ -15,7 +15,8 @@ def location_builder(filter_by: dict):
                                                                   output_field=FloatField()))
         builder = builder & Q(radius_sqr__lte=pow(rad / 9, 2))
     if 'search' in filter_by:
-        builder = builder & Q(district__icontains=filter_by['search'])
+        search = filter_by['search']
+        builder = builder & Q(district__icontains=search)
     if 'locations' in filter_by:
         builder = builder & ~Q(pk__in=filter_by.getlist('locations'))
     if 'category' in filter_by:
