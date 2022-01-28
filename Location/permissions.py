be0e9e7b7cfd0038e9a285b1d8b7175599e61330
maxwell <shaxa882@gmail.com>
2022-01-28 23:06:11 +0500

    fixing errors'

--- Location/permissions.py
+++ Location/permissions.py
@@ -5,10 +5,13 @@ class CheckPhone(permissions.BasePermission):
     message = 4444
 
     def has_permission(self, request, view):
-        if request.user.is_phone_validated:
-            return True
-        request.user.phone_validation.save()
-        return False
-
+        try:
+            if request.user.is_phone_validated:
+                return True
+            request.user.phone_validation.save()
+            return False
+        except:
+            CheckPhone.message = 5555
+            return False
 
 phone_permission = [permissions.IsAuthenticated, CheckPhone]
