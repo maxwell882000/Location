d48b1a4ac68b1f0e566420d8418ec19f955e65db
m_axwel_l <shaxa882@gmail.com>
2022-06-06 18:13:19 -0700

    delete specialist

--- userApp/apps.py
+++ userApp/apps.py
@@ -5,3 +5,6 @@ class UserappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userApp'
     verbose_name = "Пользователи"
+
+    def ready(self):
+        import userApp.signals
