a5efbeacb35daef01a70909b37203eb17dc3c528
root <root@First-one.local>
2022-08-10 09:18:05 +0000

    fixing is_diactivated

--- specialistApp/serializers.py
+++ specialistApp/serializers.py
@@ -19,6 +19,7 @@ from commonApp.models import TempImage
 class SpecialistSerializer(SerializerWithUser):
     review_avg = serializers.FloatField(default=0.0)
     location = s.LocationSerializerCard()
+    is_deactivated = serializers.BooleanField(default= False)
 
     class Meta:
         model = Specialist
@@ -34,6 +35,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class SpecialistCreateSerializer(serializers.ModelSerializer):
     user = RegisterSerializer()
+    is_deactivated = serializers.BooleanField(default= False)
 
     class Meta:
         model = Specialist
@@ -73,6 +75,7 @@ class SpecialistUpdateSerializer(serializers.ModelSerializer):
     user = UserField()
     # category = CategoryField()
     image = CustomImageField()
+    is_deactivated = serializers.BooleanField(default= False)
 
     class Meta:
         model = Specialist
