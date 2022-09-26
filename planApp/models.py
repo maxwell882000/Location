e59364d5a3770e349445e27099fa5388fea5c68b
m_axwel_l <shaxa882@gmail.com>
2022-09-26 21:53:14 +0500

    verbose name added

--- planApp/models.py
+++ planApp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Plan(models.Model):
-    description = models.TextField()
-    amount = models.BigIntegerField()
-    days = models.IntegerField()
+    description = models.TextField(verbose_name="Описание тарифа")
+    amount = models.BigIntegerField(verbose_name="Количество денег в рублях")
+    days = models.IntegerField(verbose_name="На какое количество дней активируеться аккаунт")
 
     class Meta:
         verbose_name_plural = "Тарифы"
