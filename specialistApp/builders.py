7c598c2a33e006017181490b5c0acec993f2b6ad
m_axwel_l <shaxa882@gmail.com>
2022-10-11 19:42:41 +0500

    fixing builder

--- specialistApp/builders.py
+++ specialistApp/builders.py
@@ -5,4 +5,4 @@ def specialist_builder(filter_by: dict):
     specialist = Specialist.objects
     if 'location' in filter_by:
         specialist = specialist.filter(location_id=filter_by['location'])
-    return specialist.filter(days_activated__gt=-1)
+    return specialist.filter(days_activated__gt=1)
