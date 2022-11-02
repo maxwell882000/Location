ad47eb159116871d5b96b900b2a04df6fb378a6d
m_axwel_l <shaxa882@gmail.com>
2022-11-02 22:25:18 +0500

    fixed issue

--- specialistApp/serializers.py
+++ specialistApp/serializers.py
@@ -100,7 +100,7 @@ class SpecialistUpdateSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         render = super().to_representation(instance)
         render['location'] = s.LocationSerializerCard(
-            instance.location)
+            instance.location).data
         render['many_location'] = s.LocationSerializerCard(
             Location.objects.filter(id__in=render['many_location']), many=True).data
         render['category'] = CategorySerializer(
