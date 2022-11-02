897a15e34d7b74942d5b35340ce3eb01ecc0b351
m_axwel_l <shaxa882@gmail.com>
2022-11-02 23:02:43 +0500

    script written

--- specialistApp/builders.py
+++ specialistApp/builders.py
@@ -6,3 +6,11 @@ def specialist_builder(filter_by: dict):
     if 'location' in filter_by:
         specialist = specialist.filter(many_location__in=[filter_by['location']]).distinct()
     return specialist.filter(days_activated__gt=-1)
+
+
+def reRunLocations():
+    specialists = Specialist.objects.all()
+    for specialist in specialists:
+        if specialist.location != None:
+            specialist.many_location.add(specialist.location.id)
+            specialist.save()
